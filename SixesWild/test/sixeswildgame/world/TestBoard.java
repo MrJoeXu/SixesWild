16503078b1dd77694ec068f2ee98eae5c92855e9
Jiaqi <jiaqiren@hotmail.com>
2015-05-05 19:42:45 -0400

    More Test Cases Of World

--- SixesWild/test/sixeswildgame/world/TestBoard.java
+++ SixesWild/test/sixeswildgame/world/TestBoard.java
@@ -1,6 +1,9 @@
 package test.sixeswildgame.world;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 import org.junit.After;
@@ -12,18 +15,57 @@ import src.sixeswildgame.world.*;
 public class TestBoard extends TestCase{
 
 	
-	Board tb = new Board(9);
-	Tile tt = new Tile(5,2,9,9,3,1,false);
+	Board tb = new Board(2);
+	Tile tt1 = new Tile(5,2,0,0,3,1,false);
+	Tile tt2 = new Tile(4,0,0,1,4,0,false);
+	Tile tt3 = new Tile(2,1,1,0,5,2,false);
+	Tile tt4 = new Tile(5,2,1,1,3,1,false);
+	Tile tt5 = new Tile(3,1,0,0,3,1,false);
+	
 	private boolean enabled;
-	Space ts = new Space(tt, enabled);	
+	Space ts1 = new Space(tt1, enabled);	
+	Space ts2 = new Space(tt2, enabled);
+	Space ts3 = new Space(tt3, enabled);	
+	Space ts4 = new Space(tt4, enabled);
+	Space ts5 = new Space(tt5, enabled);
+	
+	ArrayList<Space> grid= new ArrayList<Space>();
 	
 	@Test
-	public void testgetDimention() {
-		assertEquals(9,tb.getDimension());	
-	}
-	
-	public void testsetDimention(){
+	public void testSetnGetDimention(){
 		tb.setDimension(8);
 		assertEquals(8,tb.getDimension());
+	}
+	
+	public void testsetGrid(){
+		grid.add(ts1);
+		grid.add(ts2);
+		grid.add(ts3);
+		grid.add(ts4);
+		
+		tb.setGrid(grid);
+		assertEquals(grid, tb.getGrid());
+	}
+	
+	public void testGetSpace(){
+		grid.add(ts1);
+		grid.add(ts2);
+		grid.add(ts3);
+		grid.add(ts4);
+		
+		tb.setGrid(grid);
+		assertEquals(ts1, tb.getSpace(0, 0));
+	}
+	
+	public void testSetTile(){
+		grid.add(ts1);
+		grid.add(ts2);
+		grid.add(ts3);
+		grid.add(ts4);
+		
+		tb.setGrid(grid);
+		
+		tb.setTile(0, 0, tt5);
+		assertEquals(true, tt5 != tb.getSpace(0,0).getTile());
 	}
 }
--- SixesWild/test/sixeswildgame/world/TestMove.java
+++ SixesWild/test/sixeswildgame/world/TestMove.java
@@ -45,5 +45,6 @@ public class TestMove extends TestCase{
 	}
 	
 	
+	
 
 }
