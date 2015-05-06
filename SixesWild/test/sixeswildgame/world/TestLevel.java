b7e849d8b2ac9ba2115799f7facd448105d3880c
Jiaqi <jiaqiren@hotmail.com>
2015-05-05 20:13:16 -0400

    Again

--- SixesWild/test/sixeswildgame/world/TestLevel.java
+++ SixesWild/test/sixeswildgame/world/TestLevel.java
@@ -1,6 +1,9 @@
 package test.sixeswildgame.world;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
@@ -13,11 +16,22 @@ import src.sixeswildgame.world.Tile;
 
 public class TestLevel extends TestCase{
 
-	Board tb1 = new Board(8);
-	Board tb = new Board(9);
-	Tile tt = new Tile(5,2,9,9,3,1,false);
+	Board tb1 = new Board(3);
+	Board tb = new Board(2);
+	Tile tt1 = new Tile(5,2,0,0,3,1,false);
+	Tile tt2 = new Tile(4,0,0,1,4,0,false);
+	Tile tt3 = new Tile(2,1,1,0,5,2,false);
+	Tile tt4 = new Tile(5,2,1,1,3,1,false);
+	Tile tt5 = new Tile(3,1,0,0,3,1,false);
+	
 	private boolean enabled;
-	Space ts = new Space(tt, enabled);
+	Space ts1 = new Space(tt1, enabled);	
+	Space ts2 = new Space(tt2, enabled);
+	Space ts3 = new Space(tt3, enabled);	
+	Space ts4 = new Space(tt4, enabled);
+	Space ts5 = new Space(tt5, enabled);
+	
+	ArrayList<Space> grid= new ArrayList<Space>();
 	Level tll = new Level(tb,1,"1");
 	
 	@Test
@@ -43,18 +57,18 @@ public class TestLevel extends TestCase{
 	
 	public void testInitialize(){
 		tll.initialize(tll.getGameType());
-		assertEquals(9,tll.getBoard().getDimension());
+		assertEquals(2,tll.getBoard().getDimension());
 	}
 	
 	public void testDecnIncTime(){
 		tll.setMinutes(1);
-		tll.setSeconds(1);
+		tll.setSeconds(0);
 		tll.decrementTime();
-		assertEquals(1, tll.getMinutes());
-		assertEquals(0, tll.getSeconds());
+		assertEquals(0, tll.getMinutes());
+		assertEquals(59, tll.getSeconds());
 		tll.incrementTime();
 		assertEquals(1, tll.getMinutes());
-		assertEquals(1, tll.getSeconds());
+		assertEquals(0, tll.getSeconds());
 	}
 	
 	public void testSetSpecialMove(){
@@ -82,8 +96,15 @@ public class TestLevel extends TestCase{
 		assertEquals("test", tll.getName());
 	}
 	
-	public void testgetMove(){
-		Move tm = new Move(tt,tll,1);
+	public void testSetnGetMove(){
+		grid.add(ts1);
+		grid.add(ts2);
+		grid.add(ts3);
+		grid.add(ts4);
+		tb.setGrid(grid);
+		
+		Move tm = new Move(tt1,tll,1);
+		
 		assertEquals(tm, tll.getMove());
 	}
 	
